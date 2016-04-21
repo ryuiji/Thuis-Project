040ff28276868e163c9876aca5a9c9127d3389a8
Rief Haalboom <superrief@hotmail.com>
2016-04-21 13:54:46 +0200

    commit

---
+++ Thuis Opdracht Shooting Range/Assets/Scripts/Moving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+
+    public float moveSpeed;
+    public float runSpeed;
+
+
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
